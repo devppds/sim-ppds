9fec2312b6b96ccb064045d49d747d32a5d3293c
develzy <develzy@gmail.com>
2026-06-20 14:26:39 +0700

    chore: remove old temporary utility and query scripts

--- dump.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM jabatan;
-SELECT * FROM users;
--- query.sql
+++
@@ -1,1 +0,0 @@
-SELECT u.username, u.role, u.jabatan_id, j.id as j_id, j.name, j.akses FROM users u LEFT JOIN jabatan j ON u.role = j.name;
